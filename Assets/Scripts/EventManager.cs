31b956fc95ba86c5a1b4a0b5c16ef2925cbd9634
Malachi Munro <98977314+Bluffpenguin@users.noreply.github.com>
2026-02-17 20:48:33 -0700

    Dialogue and Audio

---
+++ Assets/Scripts/EventManager.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+static public class EventManager 
+{
+    // Dialogue Menu
+    static public UnityEvent<Dialogue> OpenDialogueBox = new(); // Takes a dialogue scriptable object
+    static public UnityEvent CloseDialogueBox = new();
+
+    // Dialogue Choices
+    static public UnityEvent test = new();
+}
