f9543d780ed072bacbb46136a4bef298362c2b66
John776189 <John.hernandez7761@gmail.com>
2026-02-18 17:23:20 -0700

    movement changes

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,22 +1,31 @@
+using UnityEditor.Callbacks;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class Movement : MonoBehaviour
 {
     public InputAction MoveAction;
+    public float speed = 5f;
+    private Rigidbody2D rb;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         MoveAction.Enable();
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
-    void Update()
+
+   void FixedUpdate()
     {
         Vector2 move = MoveAction.ReadValue<Vector2>();
-        Debug.Log(move);
-        Vector2 position = (Vector2)transform.position + move * 0.04f;
-        transform.position = position;
+
+        rb.linearVelocity = move * speed;
+        
+        //Debug.Log(move);
+        //Vector2 position = (Vector2)transform.position + move * 0.04f;
+        //transform.position = position;
 
     }
 }
